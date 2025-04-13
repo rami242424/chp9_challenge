df4967c90ecfbf06b84e98acc99edf6c211f768a
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-14 00:47:13 +0900

    after circle

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import styled from "styled-components";
-import Circle from "./Circle";
 
 
 function App() {
   return (
     <div>
-      <Circle bgColor="teal" borderColor="red"/>
-      <Circle bgColor="blue"/>
-      <Circle bgColor="purple"/>
     </div>
   );
 }
