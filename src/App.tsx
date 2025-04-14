64ac3a20236aed4d155538ad18cf4274b2fabc63
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-15 01:41:22 +0900

    3.5 Form : event:React.FormEvent<HTMLInput or FormElement

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // console.log(event.currentTarget.value);
+    const {currentTarget: {value}} = event;
+    setValue(value); // setValue(event.currentTarget.value)
+  };
+
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
     <div>
+      <form onSubmit={onSubmit}>
+        <input 
+        onChange={onChange}
+        value={value} type="text" placeholder="userName" />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
