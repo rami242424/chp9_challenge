2fbcf5e198a01c60eebb927e6e61d1a5adf349b7
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-16 12:32:16 +0900

    5.6 Data Types : 콘솔 temp1을 이용하여 데이터 가공하기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -33,6 +33,73 @@ interface RouteState {
     name: string,
 }
 
+interface ITag {
+    coin_counter : number;
+    ico_counter : number;
+    id : string;
+    name : string;
+}
+
+interface IInfoData {
+    id : string;
+    name : string;
+    symbol : string;
+    rank : number;
+    is_new : boolean;
+    is_active : boolean;
+    type : string;
+    logo : string;
+    tags : ITag[];
+    description : string;
+    message : string;
+    open_source : boolean;
+    started_at : string;
+    development_status : string;
+    hardware_wallet : boolean;
+    proof_type : string;
+    org_structure : string;
+    hash_algorithm : string;
+    first_data_at : string;
+    last_data_at : string;
+}
+
+
+interface IPriceData {
+    id : string;
+    name : string;
+    symbol : string;
+    rank : number;
+    total_supply : number;
+    max_supply : number;
+    beta_value : number;
+    first_data_at : string;
+    last_updated : string;
+    quotes : {
+        USD: {
+            ath_date: string;
+            ath_price: number;
+            market_cap: number;
+            market_cap_change_24h: number;
+            percent_change_1h: number;
+            percent_change_1y: number;
+            percent_change_6h: number;
+            percent_change_7d: number;
+            percent_change_12h: number;
+            percent_change_15m: number;
+            percent_change_24h: number;
+            percent_change_30d: number;
+            percent_change_30m: number;
+            percent_from_price_ath: number;
+            price: number;
+            volume_24h: number;
+            volume_24h_change_24h: number;
+        };
+    }
+}
+
+
+
+
 
 function Coin () {
     // 방법1
@@ -40,15 +107,15 @@ function Coin () {
     // console.log(params);
     // return <h1>i am coin 💥💥{params.coinId}</h1>
     // 방법2
-    const [loading, setLoading] = useState(true);
+const [loading, setLoading] = useState(true);
     const { coinId } = useParams<ICoinProps>();
     // 방법1
     //const location = useLocation<RouteState>();
     //console.log(location);
     // 방법2
     const { state } = useLocation<RouteState>();
-    const [info, setInfo] = useState({});
-    const [priceInfo, setPriceInfo] = useState({});
+    const [info, setInfo] = useState<IInfoData>();
+    const [priceInfo, setPriceInfo] = useState<IPriceData>();
 
     useEffect(() => {
         (async() => {
@@ -61,7 +128,9 @@ function Coin () {
 
             const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
             setPriceInfo(priceData);
+            // console.log(priceData, "priceData값");
             
+            setLoading(false);
 
 
 
@@ -75,7 +144,7 @@ function Coin () {
                 {/* <Title>{location.state.name}</Title> */}
                 <Title>{ state?.name || "Loading..." }</Title>
             </Header>
-            {loading ? <Loader>...loading</Loader> : null}
+            {loading ? <Loader>loading...</Loader> : null}
         </Container>
     );
 }
