b0314f12bfbc9cd576f5ca5e714aa3b531e7ba67
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-17 11:08:16 +0900

    6강 before react-hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import ToDoList from "./ToDoList";
 
 
 const GlobalStyle = createGlobalStyle`
@@ -71,6 +72,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
+      <ToDoList />
     </>
   );
 }
