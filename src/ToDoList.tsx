fb29659cfb2a67bcfab4de46646d580be092e1b3
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-18 11:17:50 +0900

    6강 before react hook form : error setup

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,16 +2,21 @@ import { useState } from "react";
 
 function ToDoList(){
     const [toDo, setToDo] = useState("");
+    const [toDoError, setToDoError] = useState("");
     const onChange = (event:React.FormEvent<HTMLInputElement>) => {
         const {
             currentTarget : {value},
         } = event;
+        setToDoError("");
         setToDo(value);
     };
 
     const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log(toDo);
+        if(toDo.length < 10)  {
+            return setToDoError("To Do should be longer");
+        }
+        console.log("success to submit");
     };
 
     return (
@@ -19,6 +24,7 @@ function ToDoList(){
             <form onSubmit={onSubmit}>
                 <input onChange={onChange} value={toDo} placeholder="Write a to do" />
                 <button>Add</button>
+                {toDoError !== "" ? toDoError : null}
             </form>
         </div>
     );
