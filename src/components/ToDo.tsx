e28d14dfd4aa01e7e49e3b4ecc7b934d0338ae3b
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-21 21:42:39 +0900

    6.13 categories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,15 +1,35 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
 
 
 
-function ToDo({ text } : IToDo){
+function ToDo({ text, category, id } : IToDo){
+    // 방법1
+    /* const onClick = (newCategory: IToDo["category"]) => {
+        console.log("i wanna go to ", newCategory);
+    }
     return (
         <li>
             <span>{text}</span>
-            <button>TO DO</button>
-            <button>DOING</button>
-            <button>DONE</button>
+            { category !== "TO_DO" && <button onClick={() => onClick("TO_DO")}>TO DO</button>}
+            { category !== "DOING" && <button onClick={() => onClick("DOING")}>DOING</button>}
+            { category !== "DONE" && <button onClick={() => onClick("DONE")}>DONE</button>}
+        </li>
+    ); */
+
+    // 방법2
+    const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {
+        //console.log("i wanna go ", event.currentTarget.name);
+        const { currentTarget : {name}} = event;
+    }
+    const setToDos = useSetRecoilState(toDoState);
+    return (
+        <li>
+            <span>{text}</span>
+            { category !== "TO_DO" && <button name="TO_DO" onClick={onClick}>TO DO</button>}
+            { category !== "DOING" && <button name="DOING" onClick={onClick}>DOING</button>}
+            { category !== "DONE" && <button name="DONE" onClick={onClick}>DONE</button>}
         </li>
     );
 }
