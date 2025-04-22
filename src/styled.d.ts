235036737d30d4514dfa799066593e05ee44ff48
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-23 07:26:20 +0900

    #8.2 Basic Animations- part one

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,11 +1,5 @@
 import "styled-components";
 
-// and extend them!
 declare module "styled-components" {
-    export interface DefaultTheme {
-        bgColor: string;
-        textColor: string;
-        accentColor: string;
-        mode : "light" | "dark";
-    }
+  export interface DefaultTheme {}
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,3 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme : DefaultTheme = {
-    bgColor: "#353b48",
-    textColor: "#dcdde1",
-    accentColor: "#8c7ae6",
-    mode: "dark",
-}
+export const darkTheme: DefaultTheme = {};
