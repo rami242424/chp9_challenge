f477c5ffa634da0369a64df112fd2d1f7f3c5601
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-23 10:17:16 +0900

    8.7 MotionValues part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion,  } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -9,43 +9,41 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
-  hover : {
-    scale : 1.5, 
-    rotateZ : 90
-  },
-  click : {
-    scale : 1, 
-    borderRadius : "100px"
-  },
-  drag : {
-    backgroundColor: "rgb(46,204,113)",
-    transition: {
-      duration : 0.1,
-    }
+  hover: { rotateZ: 90 },
+  click: { borderRadius: "100px" },
 
-  }
 };
 
 function App() {
+  const x = useMotionValue(0);
+  useEffect(() => {
+    x.onChange(() => console.log(x.get()));
+  }, [x]);
+
   return (
     <Wrapper>
-      <Box 
-        drag
-        whileDrag="drag"
-        variants={boxVariants}
-        whileHover="hover"
-        whileTap="click"
-        //whileHover={{ scale : 1.5, rotateZ : 90}} 
-        //whileTap={{ scale : 1, borderRadius : "100px"}} 
+      <Box style={{x}}
+        drag="x"
+        dragSnapToOrigin
       />
     </Wrapper>
   );
