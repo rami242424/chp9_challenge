b462cd5175262b96d1081e07f6587abd5a1850fc
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-23 22:07:49 +0900

    8.12 Slider part One

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,10 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: linear-gradient(135deg, rgb(238,0,153), rgb(221,0,238));
-`;
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgb(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 `;
 
 const Box = styled(motion.div)`
@@ -28,32 +19,50 @@ const Box = styled(motion.div)`
   border-radius: 40px;
   position: absolute;
   top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgb(0, 0, 0, 0.1), 0 10px 20px rgb(0, 0, 0, 0.06);
 `;
-
-
 const boxVars = {
-  initial:{
-    opacity:0,
+  invisible : {
+    x: 500,
+    opacity: 0,
     scale: 0,
   },
-  visible:{
-    opacity: 1,
+  visible: {
+    x: 0,
+    opacity:1,
     scale: 1,
-    rotateZ: 360,
+    transition:{
+      duration: 1,
+    }
   },
-  leaving:{
+  exit: {
+    x: -500,
     opacity: 0,
-    y: 20,
-  }
+    scale: 0,
+    transition:{
+      duration: 0.5,
+    },
+    rotateX: 180
+  },
+
 }
+
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const nextPlz = () => setVisible( (prev) => prev === 10 ? 10 : prev+1 );
+  
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click me</button>
-      <AnimatePresence>{showing ? <Box variants={boxVars} initial="initial" animate="visible" exit="leaving"/>  : null}</AnimatePresence>
+      <AnimatePresence>
+        {[1,2,3,4,5,6,7,8,9,10].map((i)=> (
+          visible === i ? <Box variants={boxVars} initial="invisible" animate="visible" exit="exit" key={i}> {i} </Box> : null
+        ))}
+      </AnimatePresence> 
+      <button onClick={nextPlz}>Next</button>
     </Wrapper>
   );
 }
