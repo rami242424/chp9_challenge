81e5c44606ba4fb5b0ee3f82c27998b060c27706
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-23 20:49:23 +0900

    #8.9 MotionValues part Three

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import { motion, useMotionValue, useTransform, useViewportScroll } from "framer-motion";
 import { useEffect, useRef } from "react";
 
 const Wrapper = styled(motion.div)`
-  height: 100vh;
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -33,16 +33,20 @@ const Box = styled(motion.div)`
 function App() {
   const x = useMotionValue(0);
   const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
-  const gradient = useTransform(x, [-800, 0, 800], [
+  const gradient = useTransform(x, [-800, 800], [
     "linear-gradient(135deg, rgb(124, 236, 19), rgb(248, 255, 32)",
-    "linear-gradient(135deg, rgb(238,0,153), rgb(221,0,238))",
-    "linear-gradient(135deg, rgb(39, 124, 181), rgb(21, 204, 237)"
+    "linear-gradient(135deg, rgb(207, 18, 163), rgb(237, 21, 46)"
   ]);
+  const {scrollYProgress} = useViewportScroll();
+  // useEffect(() => {
+  //   scrollY.onChange(() => console.log(scrollY.get(),scrollYProgress.get()));
+  // }, [scrollY, scrollYProgress])
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 3]);
   return (
     <Wrapper style={{ background: gradient}}>
         <Box
           drag="x"
-          style={{ x, rotateZ}}
+          style={{ x, rotateZ, scale }}
         />
   
     </Wrapper>
