75e6d414bb1a6c41cb45c9fabc6e0ca0f9d2ebcb
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-25 07:01:10 +0900

    9.1

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,11 +4,17 @@ const Nav = styled.nav`
     display: flex;
     justify-content: space-between;
     align-items : center;
-
+    position: fixed;
+    width: 100%;
+    top:0;
+    background-color: red;
+    height: 80px;
+    font-size: 14px;
 `;
 
 const Col = styled.div`
-
+    display: flex;
+    align-items: center;
 `;
 
 const Logo = styled.svg`
