f0170eca4cc4436ae0de202cd733f470502cc983
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-25 20:02:57 +0900

    9.12 scrollY로 사용자의 위치를 감지해서 modal 띄우기

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import { getMovies, IGetMoviesResult } from "../api";
 import styled from "styled-components";
 import { makeImgPath } from "../utils";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, useViewportScroll } from "framer-motion";
 import { useState } from "react";
 import { useHistory, useRouteMatch } from "react-router-dom";
 
@@ -126,11 +126,12 @@ const InfoVars = {
 }
 
 const Overlay = styled(motion.div)`
-    position: absolute;
+    position: fixed;
     top: 0;
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
+    opacity:0;
 `;
 
 
@@ -140,7 +141,7 @@ const offset = 6;
 function Home(){
     const history = useHistory();
     const bigMovieMatch = useRouteMatch<{ movieId: string }>("/movies/:movieId");
-    console.log(bigMovieMatch);
+    const {scrollY} = useViewportScroll();
     const { data , isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
     //console.log(data, isLoading);
     const [index, setIndex] = useState(0);
@@ -209,7 +210,7 @@ function Home(){
                     <AnimatePresence>
                         {bigMovieMatch ? (
                             <>
-                                <Overlay onClick={onOverlayClick} />
+                                <Overlay onClick={onOverlayClick} animate={{opacity: 1}} exit={{opacity: 0}}/>
                                 <motion.div
                                     layoutId={bigMovieMatch.params.movieId}
                                     style={{
@@ -217,7 +218,7 @@ function Home(){
                                         width: "40vw",
                                         height: "80vh",
                                         backgroundColor: "red",
-                                        top: 50,
+                                        top: scrollY,
                                         left: 0,
                                         right: 0,
                                         margin: "0 auto",
