eecb2858f3a51bae19bdebbc2780ef9b65c8bde9
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-25 18:11:45 +0900

    #9.8 Slider part Two �

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -45,31 +45,35 @@ const Slider = styled.div`
 
 const Row = styled(motion.div)`
     display:grid;
-    gap: 10px;
+    gap: 5px ;
     grid-template-columns: repeat(6, 1fr);
     position: absolute;
     width: 100%;
 
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{bgPhoto:string}>`
     background-color:  white;
     height: 200px;
-    color: red;
+    background-image: url(${(props) => props.bgPhoto});
+    background-size: cover;
+    background-position: center center;
     font-size: 66px;
 `;
 
 const rowVars = {
     hidden: {
-        x: window.outerWidth - 10,
+        x: window.outerWidth - 5,
     },
     visible : {
         x: 0,
     },
     exit : {
-        x: -window.outerWidth + 10,
+        x: -window.outerWidth + 5,
     }
-}
+};
+
+const offset = 6;
 
 function Home(){
     const { data , isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
@@ -78,9 +82,13 @@ function Home(){
     const [leaving, setLeaving] = useState(false);
 
     const increasingIdx = () => {
-        if (leaving) return; // slider가 돌아가는 중이면 동작x
-        toggleLeaving();
-        setIndex((prev) => prev + 1);
+        if( data ){
+            if (leaving) return; // slider가 돌아가는 중이면 동작x
+            toggleLeaving();
+            const totalMovies = data.results.length - 1;
+            const maxIndex = Math.floor(totalMovies / offset) - 1;
+            setIndex((prev) => prev === maxIndex ? 0 : prev + 1);
+        }
     }
     const toggleLeaving = () => setLeaving((prev) => !prev);
 
@@ -95,14 +103,24 @@ function Home(){
                         <Overview>{data?.results[0].overview}</Overview>
                     </Banner>
                     <Slider>
-                        <AnimatePresence onExitComplete={toggleLeaving}>
+                        <AnimatePresence 
+                            initial={false} // 처음 mount될때 오른쪽에서 슬라이딩 되는 것 방지
+                            onExitComplete={toggleLeaving}>
                             <Row 
                                 key={index} 
                                 variants={rowVars} initial="hidden" animate="visible" 
                                 exit="exit"
                                 transition={{ type: "tween", duration : 1 }}
                             >
-                                {[1,2,3,4,5,6].map((i) => <Box key={i}>{i}</Box>)}
+                                {data?.results
+                                    .slice(1)
+                                    .slice(offset*index, offset*index+offset)
+                                    .map((movie) => (
+                                        <Box 
+                                            key={movie.id} 
+                                            bgPhoto={makeImgPath(movie.backdrop_path, "w500")}
+                                        />
+                                    ))}
                             </Row>
                         </AnimatePresence>
                     </Slider>
