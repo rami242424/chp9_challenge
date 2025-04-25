07861934b96790d08cc9cf539b458c79516c88bb
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-25 12:01:55 +0900

    9.2 whileHover

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,7 +1,6 @@
 function Home(){
     return (
-        <div style={{ backgroundColor: "black", height: "200vh"}}>
-        </div>
+        <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>home</div>
     );
 }
 
