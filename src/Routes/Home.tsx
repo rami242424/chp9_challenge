c3ec115c860b5231b212c628e8302dbd33939708
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-25 16:50:39 +0900

    #9.6 Home Screen part Two  : api로 data 받아오기

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,16 +1,57 @@
 import { useQuery } from "react-query";
-import { getMovies } from "../api";
+import { getMovies, IGetMoviesResult } from "../api";
+import styled from "styled-components";
+import { makeImgPath } from "../utils";
 
 
+const Wrapper = styled.div`
+    background: black;
+`;
 
+const Loader = styled.div`
+    height: 20vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
+const Banner = styled.div<{bgPhoto:string}>`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 60px;
+    background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,1))  , url(${(props) => props.bgPhoto});
+    /* background-image:url(${(props) => props.bgPhoto}); */
+    background-size: cover;
+`;
 
+const Title = styled.h2`
+    font-size: 40px;
+    margin-bottom: 15px;
+`;
+
+const Overview = styled.p`
+    font-size: 20px;
+    width: 50%;
+`;
 
 function Home(){
-    const { data , isLoading } = useQuery(["movies", "nowPlaying"], getMovies);
+    const { data , isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
     console.log(data, isLoading);
     return (
-        <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>home</div>
+        <Wrapper>
+            {isLoading ? (
+                <Loader>Loading..</Loader>
+            ) : (
+                <>
+                    <Banner bgPhoto={makeImgPath(data?.results[0].backdrop_path || "")}>
+                        <Title>{data?.results[0].title}</Title>
+                        <Overview>{data?.results[0].overview}</Overview>
+                    </Banner>
+                </>
+            )}
+        </Wrapper>
     );
 }
 
